6a5efd270ed65a5d95ab3c17ba8f14d3b1899296
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-04-06 19:01:43 +0900

    데이터 타입 학습

---
+++ learn/data_type.js
@@ -0,0 +1,26 @@
+console.log('hete');
+
+// # 변수
+let testValue = 1;
+console.log(typeof testValue);
+
+// null
+let testValue2 = 2;
+console.log(typeof testValue2, testValue2);
+// 결과 : object, null
+
+// Symbol
+let testSymbol1 = Symbol(1);
+let testSymbol2 = Symbol(1);
+console.log(typeof testSymbol1, testSymbol1);
+console.log(typeof testSymbol2, testSymbol2);
+
+// 함수
+function printName(name) {
+    console.log(name);
+}
+
+// Arraw 함수
+let func = name => 'Hello, ' + name;
+console.log(func('종혁'));
+
